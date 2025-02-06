9708e1cfdd9bc644d2ecd88dfa43530f1a7a2fd6
AnthonyShea <agshea25@gmail.com>
2025-02-05 21:58:08 -0700

    folder restructure

--- api/index.py
+++ api/index.py
@@ -1,6 +1,12 @@
-# vercel/api/index.py
+# api/index.py
 from fastapi import FastAPI
 from mangum import Mangum
+import sys
+import os
+
+# Add the root directory to Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from fastapi_pydantic_mongo_beanie_anthony_shea.main import app
 
 # Create the handler for AWS Lambda
