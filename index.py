ffce7906d888d1e72fc5da398c7e07a0be841c71
AnthonyShea <agshea25@gmail.com>
2025-02-05 22:22:17 -0700

    removed api folder

--- index.py
+++ index.py
@@ -1,7 +1,6 @@
-# api/index.py
-import sys
+# index.py
 from mangum import Mangum
 from fastapi_pydantic_mongo_beanie_anthony_shea.main import app
 
-# Create the handler for AWS Lambda
 handler = Mangum(app, lifespan="off")
+
