b470766885bb07681e9a5d986be5ae4d8bfbdc5d
AnthonyShea <agshea25@gmail.com>
2025-02-05 22:10:51 -0700

    new index.py

--- api/index.py
+++ api/index.py
@@ -1,14 +1,7 @@
 # api/index.py
-from fastapi import FastAPI
-from mangum import Mangum
 import sys
-import os
-
-# Add the root directory to Python path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+from mangum import Mangum
 from fastapi_pydantic_mongo_beanie_anthony_shea.main import app
 
 # Create the handler for AWS Lambda
-handler = Mangum(app)
-
+handler = Mangum(app, lifespan="off")
